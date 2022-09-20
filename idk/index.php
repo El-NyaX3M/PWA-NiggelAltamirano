4b494b94e5c674328894daa41b2038e749d2150f
Niggel A <56854482+El-NyaX3M@users.noreply.github.com>
2022-09-20 10:04:07 -0600

    Actividad 20/09/2022

--- idk/index.php
+++ idk/index.php
@@ -6,29 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="public/css/main.css">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class= "container" >
-        <form>
-            <fieldset>
-                <legend>
-                    Datos de acceso
-                </legend>
-                <label for="">
-                    Email
-                </label>
-                <input type="text" placeholder="write here">
+    <div class= "container container-sm mt-8" >
+        <div class="row align-items-center d-flex justify-content-center">
+            <form class="col-auto">
+                <fieldset>
+                    <legend>
+                        Datos de acceso
+                    </legend>
+                    <label for="">
+                        Email
+                    </label>
+                    <input type="text" placeholder="write here">
 
-                <label for="">
-                    password
-                </label>
-                <input type="password" placeholder="* * * * *">
+                    <label for="">
+                        password
+                    </label>
+                    <input type="password" placeholder="* * * * *">
 
-                <button type = "submit">
-                    Acceder
-                </button>
-            </fieldset>
-        </form>
+                    <button type = "submit">
+                        Acceder
+                    </button>
+                </fieldset>
+            </form>
+        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
