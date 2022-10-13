7f0204a0cc55e8bd89469b9104535f5c34fba3e1
Niggel A <56854482+El-NyaX3M@users.noreply.github.com>
2022-10-13 09:51:46 -0600

    Actividad 13/10/2022

---
+++ pruebaLaravel/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('saludo/{name}', function($name){
+    echo "Hola ".$name;
+});
+
+Route::get('suma/{n1}/{n2}/{n3?}', function($n1,$n2,$n3=0){
+    echo $n1 + $n2 + $n3;
+})->where(['n1' => '[0-9]+','n2' => '[0-9]+']);
+
+Route::post('suma/', function(Request $request){
+
+});
