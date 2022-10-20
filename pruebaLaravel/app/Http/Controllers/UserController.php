75b15f472f3e8ac2aa91eadcb547f6d889854ca5
Niggel <niggel.x3m@gmail.com>
2022-10-20 09:56:12 -0600

    Actividad 20/10/2022

--- pruebaLaravel/app/Http/Controllers/UserController.php
+++ pruebaLaravel/app/Http/Controllers/UserController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class UserController extends Controller
 {
     //
     public function index(){
-        echo "hola";
+        return User::all();
     }
 
     public function show($id){
