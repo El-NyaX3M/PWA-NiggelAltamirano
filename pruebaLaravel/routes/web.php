6c3e5151f6f2ae67a11f1fbe296ede77bd7e8821
El-NyaX3M <niggel.x3m@gmail.com>
2022-10-26 18:25:07 -0600

    Tarea 26/10/2022

--- pruebaLaravel/routes/web.php
+++ pruebaLaravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 /*
@@ -37,3 +38,7 @@ Route::post('users/', [UserController::class, 'store']);
 Route::get('clients/', [ClientController::class, 'index']);
 
 Route::get('clients/{id}', [ClientController::class, 'show']);
+
+Route::get('reservations/', [ReservationController::class, 'index']);
+
+Route::get('reservations/{id}', [ReservationController::class, 'show']);
